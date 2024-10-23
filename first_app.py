2c8cd69cbc678bdda1c7c663e0438b4df60b63fd
sudebalkis <sudebalkiiss@gmail.com>
2024-10-23 22:22:25 +0300

    Create first_app.py

---
+++ first_app.py
@@ -0,0 +1,8 @@
+def kullanici_bilgileri_yazdir(isim, soyisim, yas):
+    print(f"İsim: {isim}, Soyisim: {soyisim}, Yaş: {yas}")
+
+isim = input("İsim: ")
+soyisim = input("Soyisim: ")
+yas = int(input("Yaş: "))
+
+kullanici_bilgileri_yazdir(isim, soyisim, yas)
